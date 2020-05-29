e6600aa094f38c5a37e392e97e70d80f94f5dab2
Aqualio <nils.chol@sfr.fr>
2020-05-29 17:25:36 +0200

    javadoc connec

--- src/connec/ServerThread.java
+++ src/connec/ServerThread.java
@@ -3,17 +3,47 @@ import java.io.*;
 import java.net.*;
  
 /**
- * This thread is responsible to handle client connection.
+ * Class name : ServerThread 
+ * This thread is responsible to handle client connection From the server side.
+ * 
+ * Version : 1.0
+ * 
+ * Date : 27/05/2020
  */
 public class ServerThread extends Thread {
+	
+	/**
+	 * This variable countains the server port 
+	 */
     private Socket socket;
+    /**
+     * This stream will handle the objects going through the sockets
+     */
 	private ObjectInputStream input;
+	/**
+	 * This stream will handle the objects coming through the sockets
+	 */
 	private ObjectOutputStream output;
  
+	/**
+	 * This is the constructor of the class
+	 * 
+	 * @param socket Is the socket of the server 
+	 * 
+	 * @author Nils Chol; Jean-Louis Cheng
+	 *
+	 */
     public ServerThread(Socket socket) {
         this.socket = socket;
     }
  
+	/**
+	 * This function will run the client connection and executes the task leading to the exchange with the server
+	 * 
+	 * @see java Class Thread; java Class Socket; java Class ObjectInputStream; java Class ObjectOutputStream
+	 * @author Nils Chol; Jean-Louis Cheng
+	 *
+	 */
     public void run() {
         try {
 			//create the streams that will handle the objects coming through the sockets
--- src/connec/SimpleClient.java
+++ src/connec/SimpleClient.java
@@ -2,14 +2,46 @@ package connec;
 import java.io.*;  
 import java.net.*; 
 
+/**
+ * Class name : SimpleClient
+ * This thread is responsible to handle client connection from the client side.
+ * 
+ * Version : 1.0
+ * 
+ * Date : 29/05/2020
+ */
 public class SimpleClient {
 	
+    /**
+     * This stream will handle the objects going through the sockets
+     */
 	private ObjectOutputStream output;
+    /**
+     * This stream will handle the objects coming through the sockets
+     */
 	private ObjectInputStream input;
+	/**
+	 * This is the socket variable
+	 */
 	private Socket socket;
+	/**
+	 * This string will contain the user's message 
+	 */
 	public String Senda;
+	/**
+	 * This boolean is used to tell if a message has been sent or not
+	 */
 	public volatile boolean messageSent = false;
 	
+	
+	/**
+	 * This function connect the client to the server 
+	 * 
+	 * @param ip is the Ip address where the server is located 
+	 * 
+	 * @author Nils Chol; Jean-Louis Cheng
+	 *
+	 */
 	public void connect(String ip)
 	{
 		int port = 6666;
